67d32a7e3cee742fc7e927c4d25a4865b21ea951
Alex Clink <alex.clink@salespad.net>
2016-03-02 13:25:02 -0500

    Add album list and artist selection

--- app/angular/controllers/libraryController.js
+++ app/angular/controllers/libraryController.js
@@ -11,11 +11,13 @@ app.controller(
   ) {
     
     $scope.artists = Artist.query();
+    $scope.selectedArtist = null;
 
     $scope.getAlbums = function(artist) {
       if (!artist.albums) {
         artist.albums = Album.byArtist({artist_id: artist.id});
       }
+      $scope.selectedArtist = artist;
     };
     
 });
